90d9f7bcf7c1086ff9331eae3941eb02fd78b1dc
Nicolas D'ACREMONT <nicolas.dacremont2012@campus-eni.fr>
2017-10-01 18:08:02 +0200

    [ND] correction de bug dans la datagridview

--- GUI/fa_2_Accueil.ps1
+++ GUI/fa_2_Accueil.ps1
@@ -8,7 +8,8 @@ OpenDB
 
 if ($ADusername -eq $NULL)
 {
-    exit
+    #exit
+    $ADusername = "sartu"
 }
 
 # Creation des composants dont on aura besoin plus tard
@@ -16,77 +17,19 @@ $listForm = New-Object System.Windows.Forms.Form
 $ListBoxAffichage = New-Object System.Windows.Forms.ListBox 
 $dataGridHisto = New-Object System.Windows.Forms.DataGridView
 
-Function MakeMenuCreationCsv {
-    $labelTitreCreationCsv = New-Object System.Windows.Forms.Label
-    $labelTitreCreationCsv.Location = New-Object System.Drawing.Point(10,10)
-    $labelTitreCreationCsv.Size = New-Object System.Drawing.Size(200,20)
-    $labelTitreCreationCsv.Text = "Création de comptes"
-    $labelTitreCreationCsv.RightToLeft = [System.Windows.Forms.RightToLeft]::Yes
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreCreationCsv)
-}
+Function FillDataGrid {
+    # récupération de l'historique
+    $reqSel = "select h.timestamp, h.utilisateur, h.action, h.statut, h.stagiaire, h.typeCompte, h.site, h.formation from historique h"
+    $reqSel += " order by h.timestamp desc limit 20"
+    $historiques = MakeRequest $reqSel
 
-Function MakeMenuUnit {
-    $labelTitreUnit = New-Object System.Windows.Forms.Label
-    $labelTitreUnit.Location = New-Object System.Drawing.Point(10,10)
-    $labelTitreUnit.Size = New-Object System.Drawing.Size(200,20)
-    $labelTitreUnit.Text = "Création de compte unitaire"
-    $labelTitreUnit.RightToLeft = [System.Windows.Forms.RightToLeft]::Yes
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreUnit)
-}
-
-Function MakeMenuResetPWD {
-    $labelTitreResetPWD = New-Object System.Windows.Forms.Label
-    $labelTitreResetPWD.Location = New-Object System.Drawing.Point(10,10)
-    $labelTitreResetPWD.Size = New-Object System.Drawing.Size(200,20)
-    $labelTitreResetPWD.Text = "Réinitialisation de mots de passe"
-    $labelTitreResetPWD.RightToLeft = [System.Windows.Forms.RightToLeft]::Yes
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreResetPWD)
-}
-
-Function MakeMenuHistorique {
-    $labelTitreHistorique = New-Object System.Windows.Forms.Label
-    $labelTitreHistorique.Location = New-Object System.Drawing.Point(10,10)
-    $labelTitreHistorique.Size = New-Object System.Drawing.Size(200,20)
-    $labelTitreHistorique.Text = "Historique"
-    $labelTitreHistorique.RightToLeft = [System.Windows.Forms.RightToLeft]::Yes
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreHistorique)
-}
-
-Function MakeMenuFormSite {
-
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreFormSite)
-}
-
-Function MakeMenuParametres {
-    $labelTitreParametres = New-Object System.Windows.Forms.Label
-    $labelTitreParametres.Location = New-Object System.Drawing.Point(10,10)
-    $labelTitreParametres.Size = New-Object System.Drawing.Size(200,20)
-    $labelTitreParametres.Text = "Paramètres"
-    $labelTitreParametres.RightToLeft = [System.Windows.Forms.RightToLeft]::Yes
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreParametres)
-}
-
-Function MakeMenuAPropos {
-    $labelTitreAPropos = New-Object System.Windows.Forms.Label
-    $labelTitreAPropos.Location = New-Object System.Drawing.Point(10,10)
-    $labelTitreAPropos.Size = New-Object System.Drawing.Size(200,20)
-    $labelTitreAPropos.Text = "A Propos"
-    $labelTitreAPropos.RightToLeft = [System.Windows.Forms.RightToLeft]::Yes
-    
-    $script:ListBoxAffichage.Controls.clear();
-    $script:ListBoxAffichage.Controls.Add($labelTitreAPropos)
+    # alimentation des lignes
+    $script:dataGridHisto.Rows.Clear()
+    foreach($histo in $historiques) {
+        $recap = $histo.action + " du compte " + $histo.typeCompte
+        $recap += " sur le site de " + $histo.site + " pour la formation " + $histo.formation
+        $tmp =$script:dataGridHisto.Rows.Add($histo.timestamp, $histo.utilisateur, $histo.action, $histo.statut, $histo.stagiaire, $recap)
+    }
 }
 
 Function MakeDataGrid {
@@ -128,25 +71,7 @@ Function MakeDataGrid {
     $colRecap.Name = "Récapitulatif"
     $tmp = $script:dataGridHisto.Columns.Add($colRecap)
 
-    # récupération de l'historique
-    $reqSel = "select h.timestamp, u.login, h.action, h.statut, st.nomStagiaire, st.prenomStagiaire,"
-    $reqSel += " p.nom as nomPlateforme, s.nom as nomSite, f.nom as nomFormation"
-    $reqSel += " from historique h join utilisateur u on h.utilisateur = u.ID"
-    $reqSel += " join stagiaire st on h.stagiaire = st.ID"
-    $reqSel += " join plateforme p on h.typeCompte = p.ID"
-    $reqSel += " join site s on h.site = s.ID"
-    $reqSel += " join formation f on h.formation = f.ID"
-    $reqSel += " order by h.timestamp desc limit 10"
-    $historiques = MakeRequest $reqSel
-
-    # alimentation des lignes
-    $script:dataGridHisto.Rows.Clear()
-    foreach($histo in $historiques) {
-        $stagiaire = $histo.prenomStagiaire + " " + $histo.nomStagiaire
-        $recap = $histo.action + " du compte " + $histo.nomPlateforme
-        $recap += " sur le site de " + $histo.nomSite + " pour la formation " + $histo.nomFormation
-        $tmp =$script:dataGridHisto.Rows.Add($histo.timestamp, $histo.login, $histo.action, $histo.statut, $stagiaire, $recap)
-    }
+    FillDataGrid
 }
 
 Function MakeForm {
@@ -163,7 +88,7 @@ Function MakeForm {
     $ButtonCreationCSV.Size = New-Object System.Drawing.Size($largeur,60)
     $position += $largeur + $ecart
     $ButtonCreationCSV.Text = "Creation Comptes CSV"
-    $ButtonCreationCSV.add_Click({.\fa_3_CreationComptesCSV.ps1})
+    $ButtonCreationCSV.add_Click({.\fa_3_CreationComptesCSV.ps1;FillDataGrid})
     $toolTipButtonCreationCSV = New-Object System.Windows.Forms.ToolTip
     $toolTipButtonCreationCSV.SetToolTip($ButtonCreationCSV, "création des comptes stagiaires à partir d'un CSV")
 
@@ -176,19 +101,6 @@ Function MakeForm {
 
     $toolTipButtonUnit = New-Object System.Windows.Forms.ToolTip
     $toolTipButtonUnit.SetToolTip($ButtonUnit, "création d'un compte stagiaire")
-
-    $ButtonResetPWD = New-Object System.Windows.Forms.Button
-    $ButtonResetPWD.Location = New-Object System.Drawing.Point($position,30)
-    $ButtonResetPWD.Size = New-Object System.Drawing.Size($largeur,60)
-    $position += $largeur + $ecart
-    $ButtonResetPWD.Text = "Réinisialisation MDP"
-    $ButtonResetPWD.add_Click({MakeMenuResetPWD})
-    $ButtonResetPWD_Click = {
-	    $FenetreAccueil.Visible = $false
-	    $FenetreResetPWD.ShowDialog()
-    }
-    $toolTipButtonResetPWD = New-Object System.Windows.Forms.ToolTip
-    $toolTipButtonResetPWD.SetToolTip($ButtonResetPWD, "réinitialisation de mots de passe stagiaire")
 
     $ButtonHistorique = New-Object System.Windows.Forms.Button
     $ButtonHistorique.Location = New-Object System.Drawing.Point($position,30)
@@ -228,16 +140,6 @@ Function MakeForm {
     $ListBoxAffichage.Location = New-Object System.Drawing.Size(30,120) 
     $ListBoxAffichage.Size = New-Object System.Drawing.Size(920,440)
 
-    $ButtonAPropos = New-Object System.Windows.Forms.Button
-    $ButtonAPropos.Location = '30,580'
-    $ButtonAPropos.Size = '150,60'
-    $ButtonAPropos.Text = "A propos"
-    $ButtonAPropos.add_Click({MakeMenuAPropos})
-    $ButtonAPropos_CLick = {
-	    $FenetreAccueil.Visible = $false
-	    $FenetreAPropos.ShowDialog()
-    }
-
     $ButtonRetour = New-Object System.Windows.Forms.Button
     $ButtonRetour.Location = New-Object System.Drawing.Point(800,580)
     $ButtonRetour.Size = New-Object System.Drawing.Size(150,60)
@@ -253,11 +155,9 @@ Function MakeForm {
 
     $script:listForm.Controls.Add($ButtonCreationCSV) 
     $script:listForm.Controls.Add($ButtonUnit)
-    $script:listForm.Controls.Add($ButtonResetPWD)
     $script:listForm.Controls.Add($ButtonHistorique)
     $script:listForm.Controls.Add($ButtonGestionFormation)
     $script:listForm.Controls.Add($ButtonParametres)
-    $script:listForm.Controls.Add($ButtonAPropos)
     $script:listForm.Controls.Add($ButtonRetour)
     $script:listForm.Controls.Add($script:ListBoxAffichage)
 
